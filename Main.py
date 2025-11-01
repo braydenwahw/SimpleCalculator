6fb9ba3c62a74df755adebf2746a47ce3436dbf5
Brayden Wampole <Brayden.wampole@gmail.com>
2025-11-01 01:10:45 -0500

    Added a few QOL changes

--- Main.py
+++ Main.py
@@ -1,19 +1,26 @@
-import time
+print("Welcome to SimpleCalculator!")
 
-#variables
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-operation = input("Enter the operation (-, +, *, /)\n:  ")
+while True:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    print("SimpleCalculator can only perform the following operations: (-, +, /, *)\nType 'q' to quit the program. ");
+    operation = input("Enter the operation:  ")
 
-if operation == "+":
-    print(num1 + num2)
-elif operation == "-":
-    print(num1 - num2)
-elif operation == "*":
-    print(num1 * num2)
-elif operation == "/":
-    print(num1 / num2)
-else:
-    print("Invalid operation")
+    if operation == "q":
+        break
+    elif operation == "/" and num2 == 0:
+        print("Cannot divide by zero. ")
+        print("Thank you for using SimpleCalculator :) ")
+        exit()
+    elif operation == "+":
+        print(f"Result: {num1 + num2}")
+    elif operation == "-":
+        print(f"Result: {num1 - num2}")
+    elif operation == "*":
+        print(f"Result: {num1 * num2}")
+    elif operation == "/":
+        print(f"Result: {num1 / num2}")
+    else:
+        print("Invalid operation")
 
 print("Thank you for using SimpleCalculator :) ")
